94ae31ea0d12c8c16d1c67147450eedd91c87fa8
zarkuac <tsotne.zarkua.1@btu.edu.ge>
2024-08-31 16:45:35 +0400

    tours endpoint +factory

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TravelController;
+use App\Http\Controllers\Api\V1\TourController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,4 @@ Route::get('/user', function (Request $request) {
 
 
 Route::get('travels', [TravelController::class, 'index']);
+Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
---
+++ tests/Feature/TourListTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Tour;
+use App\Models\Travel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TourListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_tours_list_by_travel_slug_returns_correct_tours(): void
+    {
+        $travel = Travel::factory()->create();
+        $tour = Tour::factory()->create(['travel_id' => $travel->id]);
+
+        $response = $this->get('/api/v1/travels'.$travel->slug.'/tours');
+
+        $response->assertStatus(200);
+        $response->assertJsoncount(1, 'data');
+        $response->assertJsonFragment(['id' => $tour->id]);
+    }
+}
