67ac43895b164eb06ac11ad3b34739f6929dd3f3
zarkuac <tsotne.zarkua.1@btu.edu.ge>
2024-08-31 22:18:27 +0400

    admin endpoint +login

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TravelController;
+use App\Http\Controllers\Api\V1\Admin;
 use App\Http\Controllers\Api\V1\TourController;
+use \App\Http\Controllers\Api\V1\Auth\LoginController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +14,10 @@ Route::get('/user', function (Request $request) {
 
 Route::get('travels', [TravelController::class, 'index']);
 Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
+
+Route::prefix('admin')->middleware(['auth:sanctum','role:admin'])->group(function () {
+    Route::post('travels', [Admin\TravelController::class, 'store']);
+});
+
+Route::post('login', LoginController::class);
+
